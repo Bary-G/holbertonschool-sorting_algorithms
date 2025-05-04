e06fa250402b97fcdb414e731a401caf1927cc67
Bary-G <b9059289@gmail.com>
2025-05-04 23:37:11 +0200

    r

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * swap - func
+ * @a: val
+ * @b: val
+ */
+void swap(int *a, int *b) {
+    if (a != b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+/**
+ * partition - func
+ * @array: val
+ * @low: val
+ * @high: val
+ * @size: val
+ */
+size_t partition(int *array, size_t low, size_t high, size_t size) {
+    int pivot = array[high];
+    size_t i = low;
+	size_t j;
+
+    for (j = low; j < high; j++) {
+        if (array[j] < pivot) {
+            swap(&array[i], &array[j]);
+            print_array(array, size);
+            i++;
+        }
+    }
+    swap(&array[i], &array[high]);
+    print_array(array, size);
+    return i;
+}
+
+/**
+ * quick_sort_helper - func
+ * @array: val
+ * @low: val
+ * @high: val
+ * @size: val
+ */
+void quick_sort_helper(int *array, size_t low, size_t high, size_t size) {
+	size_t pivot_index;
+
+	if (low < high) {
+        pivot_index = partition(array, low, high, size);
+        if (pivot_index > 0) {
+            quick_sort_helper(array, low, pivot_index - 1, size);
+        }
+        quick_sort_helper(array, pivot_index + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort_helper - func
+ * @array: val
+ * @size: val
+ */
+void quick_sort(int *array, size_t size) {
+    if (array == NULL || size < 2) {
+        return;
+    }
+    quick_sort_helper(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <unistd.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,5 +26,9 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
 void free_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+size_t partition(int *array, size_t low, size_t high, size_t size);
+void quick_sort_helper(int *array, size_t low, size_t high, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif

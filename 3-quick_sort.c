f1a22eee9fa4acf25c7fdaa93a152a1ad7370563
Bary-G <b9059289@gmail.com>
2025-05-06 09:50:22 +0200

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -35,7 +35,6 @@ size_t partition(int *array, size_t low, size_t high, size_t size)
 		{
 			i++;
 			swap(&array[i], &array[j]);
-			print_array(array, size);
 		}
 	}
 	swap(&array[i], &array[low]);

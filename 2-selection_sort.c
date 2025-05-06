e835c584d1cc6e8b739453219ebc23db4c363ce8
Bary-G <b9059289@gmail.com>
2025-05-06 10:18:39 +0200

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,29 +10,31 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int temp;
-	size_t minIndex;
-	size_t i;
-	size_t j;
-
-	if (array == NULL || size < 2)
+	if (size < 2 || array == NULL)
 	{
 		return;
 	}
 
-	for (i = 0; i < size - 1; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		minIndex = i;
+		size_t min_index = i;
+		size_t j;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[minIndex])
+			if (array[j] < array[min_index])
 			{
-				minIndex = j;
-				print_array(array, size);
+				min_index = j;
 			}
 		}
-		temp = array[minIndex];
-		array[minIndex] = array[i];
-		array[i] = temp;
+
+		if (min_index != i)
+		{
+			int temp = array[i];
+
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
 	}
 }
